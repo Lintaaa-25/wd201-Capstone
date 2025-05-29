3244a8d5807d6c9951d66dd452d9af1950dbdeb8
Lintaaa-25 <lintababu3@gmail.com>
2025-05-29 23:45:02 +0530

    Add files via upload

---
+++ migrations/20250529180911-create-quizzes.js
@@ -0,0 +1,55 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Quizzes', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      chapterId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Chapters',
+          key: 'id',
+        },
+        onDelete: 'CASCADE',
+      },
+      question: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+      },
+      optionA: {
+        type: Sequelize.STRING,
+      },
+      optionB: {
+        type: Sequelize.STRING,
+      },
+      optionC: {
+        type: Sequelize.STRING,
+      },
+      optionD: {
+        type: Sequelize.STRING,
+      },
+      correctAnswer: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Quizzes');
+  },
+};
---
+++ migrations/20250529180920-create-studentquizattempts.js
@@ -0,0 +1,48 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('StudentQuizAttempts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Users',
+          key: 'id',
+        },
+        onDelete: 'CASCADE',
+      },
+      quizId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Quizzes',
+          key: 'id',
+        },
+        onDelete: 'CASCADE',
+      },
+      selectedAnswer: {
+        type: Sequelize.STRING,
+      },
+      isCorrect: {
+        type: Sequelize.BOOLEAN,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('StudentQuizAttempts');
+  },
+};
