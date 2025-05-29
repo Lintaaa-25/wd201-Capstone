200b98635a1a65bdcf5147b7ac258374cd18c06f
Lintaaa-25 <lintababu3@gmail.com>
2025-05-29 22:47:55 +0530

    Add studentquizattempt model for tracking student quiz attempts

---
+++ models/studentquizattempt.js
@@ -0,0 +1,15 @@
+module.exports = (sequelize, DataTypes) => {
+  const StudentQuizAttempt = sequelize.define('StudentQuizAttempt', {
+    userId: DataTypes.INTEGER,
+    quizId: DataTypes.INTEGER,
+    selectedAnswer: DataTypes.STRING,
+    isCorrect: DataTypes.BOOLEAN
+  });
+
+  StudentQuizAttempt.associate = models => {
+    StudentQuizAttempt.belongsTo(models.User, { foreignKey: 'userId' });
+    StudentQuizAttempt.belongsTo(models.Quiz, { foreignKey: 'quizId' });
+  };
+
+  return StudentQuizAttempt;
+};
